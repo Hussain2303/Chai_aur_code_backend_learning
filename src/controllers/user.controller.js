4240b62ddde281823fc981f697113f3944dcedd5
Hussain <hussainajmal6780@gmail.com>
2025-08-15 02:19:04 +0500

    Add getUserChannelProfile function to fetch channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -287,6 +287,76 @@ return res.status(200).json(
 
 
 })
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+const {username}=req.params
+if(!username?.trim())
+{
+    throw new ApiError(400,"Username is required !!")
+}
+const channel =await User.aggregate([
+{
+    $match:{
+        username:username?.toLowerCase()
+    }
+},
+{
+    $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"Channel",
+        as:"Subscribers"
+    }
+},
+{
+    $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:" Subscriber",
+        as:"SubscribeTo"
+    }
+},
+{
+    $addFields:{
+        subscriberCount:{
+            $size: "$Subscribers"
+        },
+        subscribedTo:{
+$size:"$SubscribeTo"
+        },
+        isSubscribed:{
+            $cond:{
+                if:{$in:[req.user?._id,"$Subscribers.Subscriber"]},
+                then:true,
+                else:false
+            }
+        }
+
+    }
+},
+{$project:{
+    fullname:1,
+    username:1,
+    avatar:1,
+    coverImage:1,
+    subscriberCount:1,
+    subscribedTo:1,
+    isSubscribed:1,
+    email:1
+}
+}
+])
+if(!channel?.length)
+{
+throw new ApiError(404,"Channel Not Found !!")
+}
+res
+.status(200)
+.json(
+    new ApiResponse(200, channel[0], "Channel Profile Fetched Successfully !!")
+)
+})
+
 export {
     registerUser,
     LoginUser,
@@ -296,6 +366,7 @@ export {
     currentUser,
     UpdateAccountDetails,
     UpdateAvatar,
-    UpdateCoverImage
+    UpdateCoverImage,
+    getUserChannelProfile
 }
 //Will do soon Inshallah
